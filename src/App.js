d760c51507285b49a6364c41f99e7d3c9f14d9db
Ashwini <pardeshia1998@gmail.com>
2022-01-06 17:02:47 +0530

    registration

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="m-3">
+      <div style={{ fontFamily: "cursive" }}>Hello, Good Evening</div>
+      <Registration />
     </div>
   );
 }
 
-export default App;
+function Registration() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [list, setList] = useState([]);
+
+  const userVal = (e) => {
+    setUsername(e.target.value);
+  };
+  const userPass = (e) => {
+    setPassword(e.target.value);
+  };
+  const register = () => {
+    let user = { val1: username, val2: password };
+    const list1 = [user, ...list];
+    setList(list1);
+    setUsername("");
+    setPassword("");
+  };
+
+  return (
+    <div>
+      <div>
+        <input
+          type="text"
+          value={username}
+          placeholder="Enter Username..."
+          onChange={userVal}
+        />
+      </div>
+      <div>
+        <input
+          type="text"
+          placeholder="Enter Password..."
+          value={password}
+          onChange={userPass}
+        />
+      </div>
+      <div>
+        <input type="button" value="Register" onClick={register} />
+      </div>
+      {list.map((item) => (
+        <div>
+          {item.val1} {item.val2}
+        </div>
+      ))}
+    </div>
+  );
+}
